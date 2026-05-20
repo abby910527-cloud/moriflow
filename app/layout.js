143c8557f7641e1776d116d5d32af770561f2a61
abby910527-cloud <abby910527@gmail.com>
2026-05-20 19:49:25 +0800

    Add files via upload

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import './style.css';
+export const metadata={title:'MoriFlow Seller OS',description:'電商訂單、商品、出貨管理系統'};
+export default function RootLayout({children}){return <html lang="zh-Hant"><body>{children}</body></html>}
